09fb01cb18638df927599395c2fd36df7ea3291b
Tymoshenko Danylo <tymoshenko.danylo.a@gmail.com>
2023-12-22 21:35:53 +0200

    commit task 4

--- main.py
+++ main.py
@@ -30,11 +30,38 @@
 # слово для заміни. Зробіть у рядку заміну одного слова на інше.
 # Отриманий рядок на екрані.
 
+# try:
+#     text = str(input("Input text: "))
+#     search = str(input("Enter a search word: "))
+#     replacement = str(input("Enter a replacement word: "))
+#     newText = text.replace(search, replacement)
+#     print(f"New text: {newText}")
+# except Exception as Error:
+#     print(f"ExceptionError: {Error}")
+
+# 4. Дано рядок. (зробити зрізи)
+# - Спершу виведіть третій символ цього рядка.
+# - У другому рядку виведіть передостанній символ цього рядка.
+# - У третьому рядку виведіть перші п'ять символів цього рядка.
+# - У четвертому рядку виведіть весь рядок, крім двох останніх символів.
+# - У п'ятому рядку виведіть усі символи з парними індексами
+# (вважаючи, що індексація починається з 0, тому символи виводяться з першого).
+
+# - У шостому рядку виведіть усі символи з непарними індексами, тобто, починаючи з другого символу рядка.
+# - У сьомому рядку виведіть усі символи у зворотному порядку.
+# - У восьмому рядку виведіть усі символи рядка через один у зворотному порядку, починаючи з останнього.
+# - У дев'ятому рядку виведіть довжину цього рядка.
+
 try:
-    text = str(input("Input text: "))
-    search = str(input("Enter a search word: "))
-    replacement = str(input("Enter a replacement word: "))
-    newText = text.replace(search, replacement)
-    print(f"New text: {newText}")
+    text = "I love my home!"
+    print(f"1.\t{text[2]}")
+    print(f"2.\t{text[-2]}")
+    print(f"3.\t{text[0:5]}")
+    print(f"4.\t{text[:-2]}")
+    print(f"5.\t{text[::2]}")
+    print(f"6.\t{text[1::2]}")
+    print(f"7.\t{text[::-1]}")
+    print(f"8.\t{text[-1::-2]}")
+    print(f"9.\t{len(text)}")
 except Exception as Error:
     print(f"ExceptionError: {Error}")
